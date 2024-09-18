64a02e64c75186eb2577413d3699ef52da8c7e7a
JTR3267 <tzerongjian.cs13@nycu.edu.tw>
2024-09-18 17:01:04 +0000

    feat: HW2-2

---
+++ HW2-2/hw2-2-1.py
@@ -0,0 +1,79 @@
+import onnx
+
+inputs = dict()
+initializers = dict()
+value_infos = dict()
+outputs = dict()
+
+def get_value_by_key(key):
+    if key in inputs.keys():
+        return inputs[key]
+    elif key in initializers.keys():
+        return initializers[key]
+    elif key in value_infos.keys():
+        return value_infos[key]
+    elif key in outputs.keys():
+        return outputs[key]
+    else:
+        return key
+
+def parse_shape(shape):
+    dims = []
+    for dim in shape.dim:
+        if dim.HasField('dim_param'):
+            dims.append(dim.dim_param)
+        elif dim.HasField('dim_value'):
+            dims.append(dim.dim_value)
+    return dims
+
+onnx_model = onnx.load('./mobilenetv2-10.onnx')
+onnx_model = onnx.shape_inference.infer_shapes(onnx_model)
+
+for input in onnx_model.graph.input:
+    parsed_input = dict()
+    parsed_input["dim"] = tuple(parse_shape(input.type.tensor_type.shape))
+    parsed_input["elem_type"] = input.type.tensor_type.elem_type
+    inputs[input.name] = parsed_input
+
+for init in onnx_model.graph.initializer:
+    parsed_init = dict()
+    parsed_init["dim"] = tuple(init.dims)
+    parsed_init["elem_type"] = init.data_type
+    initializers[init.name] = parsed_init
+
+for value_info in onnx_model.graph.value_info:
+    parsed_value_info = dict()
+    parsed_value_info["dim"] = tuple(parse_shape(value_info.type.tensor_type.shape))
+    parsed_value_info["elem_type"] = value_info.type.tensor_type.elem_type
+    value_infos[value_info.name] = parsed_value_info
+
+for output in onnx_model.graph.output:
+    parsed_output = dict()
+    parsed_output["dim"] = tuple(parse_shape(output.type.tensor_type.shape))
+    parsed_output["elem_type"] = output.type.tensor_type.elem_type
+    outputs[output.name] = parsed_output
+
+ops = dict()
+for i in onnx_model.graph.node:
+    if i.op_type not in ops.keys():
+        ops[i.op_type] = 1
+    else:
+        ops[i.op_type] += 1
+print(f"Operator types: {ops}")
+
+operator_attributes = dict()
+for i in onnx_model.graph.node:
+    attrs = dict()
+    if "Conv" in i.name:
+        input_dims = get_value_by_key(i.input[0])["dim"]
+        attrs["Width"] = input_dims[-1]
+        attrs["Height"] = input_dims[-2]
+        attrs["Channel"] = input_dims[-3]
+    for attr in i.attribute:
+        attr_str = str(attr).strip()
+        attrs[attr.name] = ", ".join(attr_str.split("\n")[1:])
+    operator_attributes[i.name] = attrs
+print("Operator attributes")
+for key,value in operator_attributes.items():
+    print(key)
+    print(value)
---
+++ HW2-2/hw2-2-3.py
@@ -0,0 +1,112 @@
+import onnx
+
+inputs = dict()
+initializers = dict()
+value_infos = dict()
+outputs = dict()
+
+def mutiply_tuple(input_value):
+    if len(input_value["dim"]) == 0:
+        return 0
+    
+    dims = (1 if elem == 'batch_size' else elem for elem in input_value["dim"])
+    result = 1
+
+    for element in dims:
+        result *= element
+
+    match input_value["elem_type"]:
+        case 1:
+            result *= 4
+        case 2:
+            result *= 1
+        case 3:
+            result *= 1
+        case 4:
+            result *= 2
+        case 5:
+            result *= 2
+        case 6:
+            result *= 4
+        case 7:
+            result *= 8
+        case 9:
+            result *= 1
+        case 10:
+            result *= 2
+        case 11:
+            result *= 8
+        case 12:
+            result *= 4
+        case 13:
+            result *= 8
+        case 14:
+            result *= 8
+        case 15:
+            result *= 16
+        case _:
+            print("Undefined data type")
+
+    return result
+
+def get_value_by_key(key):
+    if key in inputs.keys():
+        return inputs[key]
+    elif key in initializers.keys():
+        return initializers[key]
+    elif key in value_infos.keys():
+        return value_infos[key]
+    elif key in outputs.keys():
+        return outputs[key]
+    else:
+        return key
+
+def parse_shape(shape):
+    dims = []
+    for dim in shape.dim:
+        if dim.HasField('dim_param'):
+            dims.append(dim.dim_param)
+        elif dim.HasField('dim_value'):
+            dims.append(dim.dim_value)
+    return dims
+
+onnx_model = onnx.load('./mobilenetv2-10.onnx')
+onnx_model = onnx.shape_inference.infer_shapes(onnx_model)
+
+for input in onnx_model.graph.input:
+    parsed_input = dict()
+    parsed_input["dim"] = tuple(parse_shape(input.type.tensor_type.shape))
+    parsed_input["elem_type"] = input.type.tensor_type.elem_type
+    inputs[input.name] = parsed_input
+
+for init in onnx_model.graph.initializer:
+    parsed_init = dict()
+    parsed_init["dim"] = tuple(init.dims)
+    parsed_init["elem_type"] = init.data_type
+    initializers[init.name] = parsed_init
+
+for value_info in onnx_model.graph.value_info:
+    parsed_value_info = dict()
+    parsed_value_info["dim"] = tuple(parse_shape(value_info.type.tensor_type.shape))
+    parsed_value_info["elem_type"] = value_info.type.tensor_type.elem_type
+    value_infos[value_info.name] = parsed_value_info
+
+for output in onnx_model.graph.output:
+    parsed_output = dict()
+    parsed_output["dim"] = tuple(parse_shape(output.type.tensor_type.shape))
+    parsed_output["elem_type"] = output.type.tensor_type.elem_type
+    outputs[output.name] = parsed_output
+
+max_activation = 0
+
+for i in onnx_model.graph.node:
+    if "Add" in i.name or "Reshape" in i.name:
+        sum_activation = 0
+        for input_name in i.input:
+            sum_activation += mutiply_tuple(get_value_by_key(input_name))
+        max_activation = max(max_activation, sum_activation)
+
+    for output_name in i.output:
+        max_activation = max(max_activation, mutiply_tuple(get_value_by_key(output_name)))
+
+print(f"Activation memory storage requirement: {max_activation}")
